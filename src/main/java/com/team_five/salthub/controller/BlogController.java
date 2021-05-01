701ae7bad7b99b80499771167341b7f8480847ed
zhengkaixin <1321760227@qq.com>
2021-05-02 01:17:50 +0800

    feat: 删除博客 close #26
    fix: 发布博客，用博客id查询博客

--- src/main/java/com/team_five/salthub/controller/BlogController.java
+++ src/main/java/com/team_five/salthub/controller/BlogController.java
@@ -1,21 +1,19 @@
 package com.team_five.salthub.controller;
 
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.sun.corba.se.impl.protocol.giopmsgheaders.RequestMessage;
 import com.team_five.salthub.model.Blog;
 import com.team_five.salthub.model.ResponseMessage;
+import com.team_five.salthub.model.constant.BlogStateEnum;
 import com.team_five.salthub.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @date 2021/04/26
@@ -27,9 +25,15 @@ public class BlogController {
     private BlogService blogService;
 
     @PostMapping
-    public ResponseMessage releaseBlog(@RequestBody Blog blog, @RequestParam("attachments") MultipartFile[] attachments){
-        String name = " ";//发布者的用户名
+    public ResponseMessage releaseBlog(@RequestBody Blog blog, @RequestParam("attachments") MultipartFile[] attachments) {
+        String name = StpUtil.getLoginIdAsString();//发布者的用户名
+        blog.setAuthor(name);
+        blog.setLikeNumber(Long.valueOf(0));
+        blog.setCollectionNumber(Long.valueOf(0));
+        blog.setState(BlogStateEnum.NORMAL.getId().intValue());
+
         blogService.validityCheck(blog);//检查博客合法性
+
         //处理一下文件
         blogService.insert(blog);//将博客存储到数据库中
         return ResponseMessage.success();
@@ -60,14 +64,14 @@ public class BlogController {
         return ResponseMessage.success(blogList);
     }
 
-    @PostMapping
+    @GetMapping
     public ResponseMessage searchBlogByBolgId(@RequestParam("current") int current, @RequestParam("blogId") int blogId) {
         Page<Blog> blogList = blogService.searchBlogByModuleId(Long.valueOf(blogId), Long.valueOf(current));
         return ResponseMessage.success(blogList);
 
     }
 
-    @PostMapping
+    @DeleteMapping
     public ResponseMessage deleteBlogByBlogId(@RequestParam("blogId") int blogId) {
         blogService.deleteBlogByBlogId(Long.valueOf(blogId));
         return ResponseMessage.success();
