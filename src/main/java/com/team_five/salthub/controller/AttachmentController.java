da9f0635f99e209dfc5aebb42bf889108aa22270
xydf <690054845@qq.com>
2021-05-08 21:12:19 +0800

    fix: 附件删除，发布博客 feat: 查询附件 close #29

--- src/main/java/com/team_five/salthub/controller/AttachmentController.java
+++ src/main/java/com/team_five/salthub/controller/AttachmentController.java
@@ -1,15 +1,13 @@
 package com.team_five.salthub.controller;
 
+import com.team_five.salthub.model.Attachment;
 import com.team_five.salthub.model.ResponseMessage;
 import com.team_five.salthub.service.AttachmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/blog")
+@RequestMapping("/attachment")
 public class AttachmentController {
     @Autowired
     private AttachmentService attachmentService;
@@ -20,5 +18,12 @@ public class AttachmentController {
 
         attachmentService.deleteAttachment(Long.valueOf(id));
         return ResponseMessage.success();
+    }
+    @GetMapping
+    public ResponseMessage searchAttachmentById(@RequestParam("id") int id){
+        attachmentService.attachmentIdValidityCheck(Long.valueOf(id));
+        Attachment attachment= attachmentService.searchAttachmentById(Long.valueOf(id));
+        attachment.setName("/attachment/"+attachment.getName());
+        return ResponseMessage.success(attachment);
     }
 }
--- src/main/java/com/team_five/salthub/service/AttachmentService.java
+++ src/main/java/com/team_five/salthub/service/AttachmentService.java
@@ -1,7 +1,11 @@
 package com.team_five.salthub.service;
 
+import com.team_five.salthub.model.Attachment;
+
 public interface AttachmentService {
     void attachmentIdValidityCheck(Long id);
 
     void deleteAttachment(Long id);
+
+    Attachment searchAttachmentById(Long id);
 }
